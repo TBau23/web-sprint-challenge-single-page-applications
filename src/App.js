1f91564a78513ce19cba4e6811657cbc9d87436d
Tom Bauer <tbauer1898@gmail.com>
2020-08-21 10:53:08 -0500

    basic outline complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { Switch, Route} from 'react-router-dom'
+import HomePage from './HomePage'
+import PizzaForm from './PizzaForm'
+
 
 const App = () => {
   return (
-    <>
+    <div className='app'>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+
+    
+      <Switch>
+        <Route path='/order'>
+          <PizzaForm />
+        </Route>
+
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+
+      </Switch>
+    
+
+    </div>
   );
 };
 export default App;
---
+++ src/PizzaForm.js
@@ -0,0 +1,98 @@
+import React from "react";
+
+const PizzaForm = () => {
+
+    return(
+    <form className='form-container'>
+        <div className='submit__div'>
+            <h2>Order a Pizza!</h2>
+            <button id='submitBtn'>Order</button>
+            <div className='errors'>
+                <div></div>
+            </div>
+
+            <div className='inputs'>
+                <h4>Select your Pizza Details</h4>
+
+                <label>This Pizza is for:&nbsp;
+                    <input
+                    name='name'
+                    type='text'
+
+                    />
+
+                </label>
+
+                <label>Pizza Size:&nbsp;
+                    <select
+                    name='size'
+
+                    >
+                        <option value=''>Select an Option</option>
+                        <option value='Small'>Small</option>
+                        <option value='Medium'>Medium</option>
+                        <option value='Large'>Large</option>
+                    </select>
+
+                </label>
+
+                <div className='toppings'>
+                    <h4>Toppings:</h4>
+                    <label>Pepperoni&nbsp;
+                        <input
+                        type='checkbox'
+                        name='pepperoni'
+
+                        />
+
+                    </label>
+
+                    <label>Sausage&nbsp;
+                        <input
+                        type='checkbox'
+                        name='sausage'
+
+                        />
+
+                    </label>
+
+                    <label>Green Peppers&nbsp;
+                        <input
+                        type='checkbox'
+                        name='green_peppers'
+
+                        />
+
+                    </label>
+
+                    <label>Mushrooms&nbsp;
+                        <input
+                        type='checkbox'
+                        name='mushrooms'
+
+                        />
+
+                    </label>
+
+                    <label>Pineapple&nbsp;
+                        <input
+                        type='checkbox'
+                        name='pineapple'
+
+                        />
+
+                    </label>
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+    </form>
+    )
+
+}
+
+export default PizzaForm;
